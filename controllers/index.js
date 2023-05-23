eb965c0adda466ac5e86b31e429db9afd756dcfb
zeinahares <zeinahares@gmail.com>
2023-05-23 12:14:03 +1000

    added a comment in controller index. the way the homepage route is required is incorrect. we need routes outside of the api to link it to. see comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+
+// !! this is incorrect, we need to create routes OUTSIDE of the api routes, specifically only to render the pages.
+// these routes will communicate with the api routes through fetch methods, and then pass the info to handlebars to display
 const homePageRoutes = require('./api/homePageRoutes');
 
 // to get here its just "localhost.com/"

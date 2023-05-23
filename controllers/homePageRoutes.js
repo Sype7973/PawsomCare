e57bdc23e546052da8ae1078cd2152ba0ffea3c2
zeinahares <zeinahares@gmail.com>
2023-05-23 14:53:50 +1000

    finished blog page route, and added the route of blogpage to the index in controller

--- controllers/homePageRoutes.js
+++ controllers/homePageRoutes.js
@@ -1,11 +1,17 @@
 // this page now acts as the main route for rendering all the handlebars pages.
-// in tis page, you do all the gets that you need fromt the API routes to get the info and pass it 
+// in tis page, you do all the gets that you need fromt the database info and pass it 
 // to handlebars to display.
 // API routes deal with posting and putting. 
-// The homepage route deals with getting the info from the database (not API) and rendering the page
+// The homepage route deals with getting the info from the database (not API) and rendering the home page
 
 // API saves info to the databse
 // homeroutes gets info from the database and passes it to handlebars to render (with res.render)
 
+// to get here its 'localhost.com/'
+// right now localhost.com/ doesnt show anything
+// this needs to be coded to display the homepage / slide 1 
+
 const router = require('express').Router();
 
+
+module.exports = router;

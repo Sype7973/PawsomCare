19ea475461bc16737eaa401c911df1f4465e956f
zeinahares <zeinahares@gmail.com>
2023-05-23 12:25:33 +1000

    changed position of homepage routes in the controller folder. moved it out of api and into the main controller file. added comments to what this route is meant to do. also added a comments API route folder to deal with the posting of comments into the DB. see comments in homepageRoutes.js

---
+++ controllers/homePageRoutes.js
@@ -0,0 +1,4 @@
+// this page now acts as the main route for rendering all the handlebars pages.
+// in tis page, you do all the gets that you need fromt the API routes to get the info and pass it 
+// to handlebars to display.
+// API routes deal with posting and putting. The homepage route deals with getting the info and rendering the page

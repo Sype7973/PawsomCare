b3d710d1291894fbd70e3309ef7c3e6d25977a3e
zeinahares <zeinahares@gmail.com>
2023-05-23 14:16:13 +1000

    created blogpost api route

--- controllers/api/blogPostAPIRoutes.js
+++ controllers/api/blogPostAPIRoutes.js
@@ -1,5 +1,58 @@
 const router = require('express').Router();
-const { BlogPost, comments } = require('../../models');
+const { BlogPost } = require('../../models');
 
+
+router.post('/', async (req, res) => {
+
+    try {
+        // create a new blogpost
+        const newBlog = await BlogPost.create(req.body);
+        res.status(200).json(newBlog);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+router.put('/:id', async (req, res) => {
+
+    try {
+        // update a blog by its `id` value
+        const updatedBlog = await BlogPost.update(
+            {
+                pet_category: req.body.pet_category,
+                post_type: req.body.post_type,
+                body: req.body.body,
+            },
+            {
+                where: {
+                    id: req.params.id,
+                },
+            }
+        );
+
+        res.status(200).json(updatedBlog);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+router.delete('/:id', async (req, res) => {
+    // delete blog by its `id` value
+    try {
+        const deletedBlog = await BlogPost.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        res.status(200).json(deletedBlog);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;

4fedb35280c6e2620ede82edd7b1338b50a3ee8c
unknown <adam.day7973@gmail.com>
2023-05-28 13:22:26 +1000

    updated design for edit page, added multer, styles css

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const multer = require('multer');
 
 const exphbs = require('express-handlebars');
 const session = require('express-session');
